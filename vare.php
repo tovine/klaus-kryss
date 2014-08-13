6d8b8650438a224a8af805ba57580db7e19bde04
Torbjørn Viem Ness <torbjorn@viem-it.no>
2014-08-13 15:58:42 +0200

    La til automagisk beregning av prisforslag

--- vare.php
+++ vare.php
@@ -38,11 +38,34 @@ if ($vare_id != '') {
 }
 ?>
 
+<script type="text/javascript">
+// TODO: utvid denne funksjonen til å kunne fungere med poløl og andre enheter som bare selges i hele flasker - skal reagere på hvilken kategori den gjeldende varen er
+function calculate() {
+	// Funksjon som beregner prisforslag ut fra formel (break_even = totalpris/svinnkorrigert_antall)
+	if (document.getElementById('pris_inn').value != '') {
+		var storrelsevalg = document.getElementById("flaskestr");
+		var divisor = storrelsevalg.options[storrelsevalg.selectedIndex].value;
+		var kostpris = Math.ceil(eval (document.getElementById('pris_inn').value + "/" + divisor));
+		document.getElementById('pris_break').value = kostpris;
+		// Runder prisen opp til nærmeste femmer
+		var last_digit = (kostpris % 10);
+		if (last_digit == 0);
+		else if (last_digit > 5 ) kostpris += 10 - last_digit;
+		else kostpris += 5 - last_digit;
+		document.getElementById('pris_forslag').value = kostpris;
+	}
+}
+function updatePrice() {
+	// Setter verdien i pris-feltet lik prisforslaget, dersom dette ikke er tomt
+	if (document.getElementById('pris_forslag').value != '') document.getElementById('input_pris').value = document.getElementById('pris_forslag').value
+}
+</script>
+
 <h3>Ny/endre vare</h3>
+<div style="float:left;">
 <a href='vareliste.php'>Tilbake</a><br />
-<? echo $message; ?>
-<form action='vare.php' method='post'>
-<input type='hidden' name='id' value='<?=$vare_id?>' />
+<form name="vare" action='vare.php' method='post'>
+<p><input type='hidden' name='id' value='<?=$vare_id?>' />
 <table>
 <tr><th>Navn:</th><td><input type='text' name='navn' value='<?=$vare_navn?>' /></td></tr>
 <tr><th>Kategori:</th><td><select name='kategori'>
@@ -61,10 +84,30 @@ while ($row = pg_fetch_array($kategori_result)) {
 }
 ?>
 </select></td></tr>
-<tr><th>Pris</th><td><input type='text' name='pris' size='3' value='<?=$vare_pris?>'/> Utgått? <input type="checkbox" name="slettet" value="t" <? if($vare_slettet == 't') echo "checked "; ?>/></td></tr>
+<tr><th>Pris</th><td><input type='text' id='input_pris' name='pris' size='3' value='<?=$vare_pris?>'/> Utgått? <input type="checkbox" name="slettet" value="t" <? if($vare_slettet == 't') echo "checked "; ?>/></td></tr>
 </table>
 <input type='submit' name='lagre' value='Lagre vare' />
+</p>
 </form>
+<? echo $message; ?>
+</div>
+
+<div style="float: left; padding-left: 20px">
+<h4>Beregn prisforslag (foreløpig kun for brennevin o.l.)</h4>
+<p>Flaskestørrelse: <select id='flaskestr' onChange='calculate()'>
+<?
+foreach ($polfaktor as $storrelse => $faktor) {
+	echo "<option value='$faktor'>$storrelse</option>";
+}
+?>
+</select><br />
+Pris hos vinmonopolet: <input id='pris_inn' size='4' onChange='calculate()'/></p>
+Break even: <input id='pris_break' size='4' disabled /><br />
+Prisforslag: <input id='pris_forslag' size='4' disabled />
+<button onClick="updatePrice()">Bruk</button>
+
+</form>
+</div>
 
 <?
 
