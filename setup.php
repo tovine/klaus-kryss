e1f52ddd8776e3087defc8147d7faf7d3e963de8
Torbjørn Viem Ness <torbjorn@viem-it.no>
2014-10-06 12:56:59 +0200

    Sikret setup.php så det bare kan kjøres fra kommandolinjen

--- setup.php
+++ setup.php
@@ -1,12 +1,12 @@
 <?php
-header('Content-type: text/HTML; encoding=UTF8');
-
+header('Content-type: text/HTML; charset=UTF8');
+(PHP_SAPI !== 'cli' || isset($_SERVER['HTTP_USER_AGENT'])) && die("Feil: dette skriptet må kjøres fra kommandolinjen, 'php setup.php'\n");
 include('../include/dbsetup.php');
 echo "Oppretter tabeller med riktig struktur...\n";
 
 $filename = "db_struct.sql"; 
 
-pg_query("BEGIN; COMMIT;\n" . file_get_contents($filename)) or die('Noe gikk galt: '.pg_last_error());
+//pg_query("BEGIN; COMMIT;\n" . file_get_contents($filename)) or die('Noe gikk galt: '.pg_last_error());
 
 echo "Databaseoppsett fullført!\n";
 
